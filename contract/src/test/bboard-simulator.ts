8a84ec87866669be32684ce2ecbe0aa1e4a372f3
Developer <developer@midnight.network>
2026-07-26 19:46:33 +0000

    test:

--- contract/src/test/bboard-simulator.ts
+++ contract/src/test/bboard-simulator.ts
@@ -1,69 +1,68 @@
-// This file is part of midnightntwrk/example-bboard.
+// Private Medical Research Data Exchange Simulator
 // Copyright (C) Midnight Foundation
-// SPDX-License-Identifier: Apache-2.0
-// Licensed under the Apache License, Version 2.0 (the "License");
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-import {
-  type CircuitContext,
-  QueryContext,
-  sampleContractAddress,
-  convertFieldToBytes,
-  createConstructorContext,
-  CostModel,
-} from "@midnight-ntwrk/compact-runtime";
 import {
   Contract,
-  type Ledger,
+  Ledger,
+  State,
   ledger,
 } from "../managed/bboard/contract/index.js";
-import { type BBoardPrivateState, witnesses } from "../witnesses.js";
+import {
+  BBoardPrivateState,
+  createBBoardPrivateState,
+  witnesses,
+} from "../witnesses.js";
+import {
+  CircuitContext,
+  ContractAddress,
+  ContractState,
+  CostModel,
+  QueryContext,
+  sampleContractAddress,
+  emptyZswapLocalState,
+} from "@midnight-ntwrk/compact-runtime";
+import { convertFieldToBytes } from "./utils.js";
 
-/**
- * Serves as a testbed to exercise the contract in tests
- */
 export class BBoardSimulator {
   readonly contract: Contract<BBoardPrivateState>;
   circuitContext: CircuitContext<BBoardPrivateState>;
 
-  constructor(secretKey: Uint8Array) {
-    this.contract = new Contract<BBoardPrivateState>(witnesses);
-    const {
-      currentPrivateState,
-      currentContractState,
-      currentZswapLocalState,
-    } = this.contract.initialState(
-      createConstructorContext({ secretKey }, "0".repeat(64)),
+  constructor(
+    secretKey: Uint8Array,
+    medicalCredentialSecret?: Uint8Array,
+    patientRecordKey?: Uint8Array,
+  ) {
+    this.contract = new Contract(witnesses);
+    const initialPrivateState = createBBoardPrivateState(
+      secretKey,
+      medicalCredentialSecret,
+      patientRecordKey,
     );
+    const initialContractState = this.contract.initialState({
+      initialPrivateState,
+      initialZswapLocalState: emptyZswapLocalState({ bytes: new Uint8Array(32) }),
+    });
     this.circuitContext = {
-      currentPrivateState,
-      currentZswapLocalState,
+      currentPrivateState: initialPrivateState,
+      currentZswapLocalState: initialContractState.currentZswapLocalState,
       costModel: CostModel.initialCostModel(),
       currentQueryContext: new QueryContext(
-        currentContractState.data,
+        initialContractState.currentContractState.data,
         sampleContractAddress(),
       ),
     };
   }
 
-  /***
-   * Switch to a different secret key for a different user
-   *
-   * TODO: is there a nicer abstraction for testing multi-user dApps?
-   */
-  public switchUser(secretKey: Uint8Array) {
-    this.circuitContext.currentPrivateState = {
+  public switchUser(
+    secretKey: Uint8Array,
+    medicalCredentialSecret?: Uint8Array,
+    patientRecordKey?: Uint8Array,
+  ) {
+    this.circuitContext.currentPrivateState = createBBoardPrivateState(
       secretKey,
-    };
+      medicalCredentialSecret,
+      patientRecordKey,
+    );
   }
 
   public getLedger(): Ledger {
@@ -74,24 +73,53 @@ export class BBoardSimulator {
     return this.circuitContext.currentPrivateState;
   }
 
-  public post(message: string): Ledger {
-    // Update the current context to be the result of executing the circuit.
-    this.circuitContext = this.contract.impureCircuits.post(
+  public registerDataset(title: string): Ledger {
+    this.circuitContext = this.contract.impureCircuits.registerDataset(
       this.circuitContext,
-      message,
+      title,
     ).context;
     return ledger(this.circuitContext.currentQueryContext.state);
   }
 
-  public takeDown(): Ledger {
-    this.circuitContext = this.contract.impureCircuits.takeDown(
+  public requestAccess(datasetId: Uint8Array): Ledger {
+    this.circuitContext = this.contract.impureCircuits.requestAccess(
       this.circuitContext,
+      datasetId,
     ).context;
     return ledger(this.circuitContext.currentQueryContext.state);
   }
 
-  public publicKey(): Uint8Array {
-    const sequence = convertFieldToBytes(
+  public grantPermission(datasetId: Uint8Array, researcherPk: Uint8Array): Ledger {
+    this.circuitContext = this.contract.impureCircuits.grantPermission(
+      this.circuitContext,
+      datasetId,
+      researcherPk,
+    ).context;
+    return ledger(this.circuitContext.currentQueryContext.state);
+  }
+
+  public submitAccessProof(
+    datasetId: Uint8Array,
+    patientRecordHash: Uint8Array,
+  ): Ledger {
+    this.circuitContext = this.contract.impureCircuits.submitAccessProof(
+      this.circuitContext,
+      datasetId,
+      patientRecordHash,
+    ).context;
+    return ledger(this.circuitContext.currentQueryContext.state);
+  }
+
+  public revokeAccess(datasetId: Uint8Array): Ledger {
+    this.circuitContext = this.contract.impureCircuits.revokeAccess(
+      this.circuitContext,
+      datasetId,
+    ).context;
+    return ledger(this.circuitContext.currentQueryContext.state);
+  }
+
+  public publicKey(sequenceBytes?: Uint8Array): Uint8Array {
+    const sequence = sequenceBytes ?? convertFieldToBytes(
       32,
       this.getLedger().sequence,
       "bboard-simulator.ts",
--- contract/src/test/bboard.test.ts
+++ contract/src/test/bboard.test.ts
@@ -1,17 +1,5 @@
-// This file is part of midnightntwrk/example-bboard.
+// Private Medical Research Data Exchange Smart Contract Unit Tests
 // Copyright (C) Midnight Foundation
-// SPDX-License-Identifier: Apache-2.0
-// Licensed under the Apache License, Version 2.0 (the "License");
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import { BBoardSimulator } from "./bboard-simulator.js";
 import { setNetworkId } from "@midnight-ntwrk/midnight-js-network-id";
@@ -21,126 +9,93 @@ import { State } from "../managed/bboard/contract/index.js";
 
 setNetworkId("undeployed");
 
-describe("BBoard smart contract", () => {
-  it("generates initial ledger state deterministically", () => {
-    const key = randomBytes(32);
-    const simulator0 = new BBoardSimulator(key);
-    const simulator1 = new BBoardSimulator(key);
-    expect(simulator0.getLedger()).toEqual(simulator1.getLedger());
-  });
-
-  it("properly initializes ledger state and private state", () => {
-    const key = randomBytes(32);
-    const simulator = new BBoardSimulator(key);
-    const initialLedgerState = simulator.getLedger();
-    expect(initialLedgerState.sequence).toEqual(1n);
-    expect(initialLedgerState.message.is_some).toEqual(false);
-    expect(initialLedgerState.message.value).toEqual("");
-    expect(initialLedgerState.owner).toEqual(new Uint8Array(32));
-    expect(initialLedgerState.state).toEqual(State.VACANT);
-    const initialPrivateState = simulator.getPrivateState();
-    expect(initialPrivateState).toEqual({ secretKey: key });
-  });
-
-  it("lets you set a message", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    const initialPrivateState = simulator.getPrivateState();
-    const message =
-      "Szeth-son-son-Vallano, Truthless of Shinovar, wore white on the day he was to kill a king";
-    simulator.post(message);
-    // the private ledger state shouldn't change
-    expect(initialPrivateState).toEqual(simulator.getPrivateState());
-    // And all the correct things should have been updated in the public ledger state
+describe("Private Medical Research Data Exchange Contract", () => {
+  it("properly initializes ledger state and private witness state", () => {
+    const secretKey = randomBytes(32);
+    const simulator = new BBoardSimulator(secretKey);
     const ledgerState = simulator.getLedger();
+
     expect(ledgerState.sequence).toEqual(1n);
-    expect(ledgerState.message.is_some).toEqual(true);
-    expect(ledgerState.message.value).toEqual(message);
-    expect(ledgerState.owner).toEqual(simulator.publicKey());
-    expect(ledgerState.state).toEqual(State.OCCUPIED);
+    expect(ledgerState.state).toEqual(State.NONE);
+    expect(ledgerState.datasetCount).toEqual(1n);
+    expect(ledgerState.auditLogCount).toEqual(0n);
+    expect(ledgerState.datasetTitle.is_some).toEqual(false);
   });
 
-  it("lets you take down a message", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    const initialPrivateState = simulator.getPrivateState();
-    const initialPublicKey = simulator.publicKey();
-    const message =
-      "Prince Raoden of Arelon awoke early that morning, completely unaware that he had been damned for all eternity.";
-    simulator.post(message);
-    simulator.takeDown();
-    // the private ledger state shouldn't change
-    expect(initialPrivateState).toEqual(simulator.getPrivateState());
-    // And all the correct things should have been updated in the public ledger state
+  it("allows a hospital to register a new medical research dataset", () => {
+    const hospitalKey = randomBytes(32);
+    const simulator = new BBoardSimulator(hospitalKey);
+    const datasetTitle = "Genomic Oncology Study 2026 - Anonymized Cohort A";
+
+    simulator.registerDataset(datasetTitle);
     const ledgerState = simulator.getLedger();
-    expect(ledgerState.sequence).toEqual(2n);
-    expect(ledgerState.message.is_some).toEqual(false);
-    expect(ledgerState.message.value).toEqual("");
-    // Technically the circuit doesn't clear the previous owner
-    expect(ledgerState.owner).toEqual(initialPublicKey);
-    expect(ledgerState.state).toEqual(State.VACANT);
+
+    expect(ledgerState.datasetTitle.is_some).toEqual(true);
+    expect(ledgerState.datasetTitle.value).toEqual(datasetTitle);
+    expect(ledgerState.datasetCount).toEqual(2n);
   });
 
-  it("lets you post another message after taking down the first", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    const initialPrivateState = simulator.getPrivateState();
-    simulator.post("Life before Death.");
-    simulator.takeDown();
-    const message = "Strength before Weakness.";
-    simulator.post(message);
-    // the private ledger state shouldn't change
-    expect(initialPrivateState).toEqual(simulator.getPrivateState());
-    // And all the correct things should have been updated in the public ledger state
+  it("allows a qualified researcher to submit a confidential access request", () => {
+    const hospitalKey = randomBytes(32);
+    const researcherKey = randomBytes(32);
+    const medicalCredential = randomBytes(32);
+    const datasetId = randomBytes(32);
+
+    const simulator = new BBoardSimulator(hospitalKey);
+    simulator.registerDataset("Cardiology Patient Outcomes Dataset");
+
+    simulator.switchUser(researcherKey, medicalCredential);
+    simulator.requestAccess(datasetId);
+
     const ledgerState = simulator.getLedger();
-    expect(ledgerState.sequence).toEqual(2n);
-    expect(ledgerState.message.is_some).toEqual(true);
-    expect(ledgerState.message.value).toEqual(message);
-    expect(ledgerState.owner).toEqual(simulator.publicKey());
-    expect(ledgerState.state).toEqual(State.OCCUPIED);
+    expect(ledgerState.state).toEqual(State.REQUESTED);
+    expect(ledgerState.activeResearcherPk).not.toEqual(new Uint8Array(32));
   });
 
-  it("lets a different user post a message after taking down the first", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    simulator.post("Remember, the past need not become our future as well.");
-    simulator.takeDown();
-    simulator.switchUser(randomBytes(32));
-    const message = "Joy was more than just an absence of discomfort.";
-    simulator.post(message);
-    const ledgerState = simulator.getLedger();
-    expect(ledgerState.sequence).toEqual(2n);
-    expect(ledgerState.message.is_some).toEqual(true);
-    expect(ledgerState.message.value).toEqual(message);
-    expect(ledgerState.owner).toEqual(simulator.publicKey());
-    expect(ledgerState.state).toEqual(State.OCCUPIED);
+  it("allows hospital owner to grant research permission and researcher to submit dataset access proof", () => {
+    const hospitalKey = randomBytes(32);
+    const researcherKey = randomBytes(32);
+    const medicalCredential = randomBytes(32);
+    const patientRecordKey = randomBytes(32);
+    const datasetId = randomBytes(32);
+    const patientRecordHash = randomBytes(32);
+
+    const simulator = new BBoardSimulator(hospitalKey);
+    simulator.registerDataset("Rare Neurological Disorders Cohort");
+
+    // Researcher requests access
+    simulator.switchUser(researcherKey, medicalCredential, patientRecordKey);
+    simulator.requestAccess(datasetId);
+    const researcherPk = simulator.getLedger().activeResearcherPk;
+
+    // Hospital owner grants permission
+    simulator.switchUser(hospitalKey);
+    simulator.grantPermission(datasetId, researcherPk);
+    expect(simulator.getLedger().state).toEqual(State.GRANTED);
+
+    // Researcher submits proof of access using private patient record key
+    simulator.switchUser(researcherKey, medicalCredential, patientRecordKey);
+    simulator.submitAccessProof(datasetId, patientRecordHash);
+
+    const updatedLedger = simulator.getLedger();
+    expect(updatedLedger.auditLogCount).toEqual(1n);
+    expect(updatedLedger.lastProofHash).not.toEqual(new Uint8Array(32));
   });
 
-  it("doesn't let the same user post twice", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    simulator.post(
-      "My name is Stephen Leeds, and I am perfectly sane. My hallucinations, however, are all quite mad.",
-    );
-    expect(() =>
-      simulator.post(
-        "You should know by now that I've already had greatness. I traded it for mediocrity and some measure of sanity.",
-      ),
-    ).toThrow("failed assert: Attempted to post to an occupied board");
-  });
+  it("allows hospital dataset owner to revoke access", () => {
+    const hospitalKey = randomBytes(32);
+    const researcherKey = randomBytes(32);
+    const datasetId = randomBytes(32);
 
-  it("doesn't let different users post twice", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    simulator.post("Ash fell from the sky");
-    simulator.switchUser(randomBytes(32));
-    expect(() =>
-      simulator.post("I am, unfortunately, the hero of ages."),
-    ).toThrow("failed assert: Attempted to post to an occupied board");
-  });
+    const simulator = new BBoardSimulator(hospitalKey);
+    simulator.registerDataset("Immunology Clinical Trial Dataset");
 
-  it("doesn't let users take down someone elses posts", () => {
-    const simulator = new BBoardSimulator(randomBytes(32));
-    simulator.post(
-      "Sometimes a hypocrite is nothing more than a man in the process of changing.",
-    );
-    simulator.switchUser(randomBytes(32));
-    expect(() => simulator.takeDown()).toThrow(
-      "failed assert: Attempted to take down post, but not the current owner",
-    );
+    simulator.switchUser(researcherKey, randomBytes(32));
+    simulator.requestAccess(datasetId);
+
+    simulator.switchUser(hospitalKey);
+    simulator.revokeAccess(datasetId);
+
+    expect(simulator.getLedger().state).toEqual(State.REVOKED);
   });
 });
